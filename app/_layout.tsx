20eb373c24d8fc55889a541f77b6213821bed1a8
John Mark Ovalo <john.ovalo@vocphone.com>
2025-01-19 14:41:04 +0800

    refactor: remove extra screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -34,8 +34,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
